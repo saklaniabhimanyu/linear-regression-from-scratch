646d3ecf1dd70c604e55ebeeab1de14aa8bc1c78
Abhimanyu Saklani <saklaniabhimanyu@gmail.com>
2026-07-17 12:10:13 +0530

    Implement Batch Gradient Descent regression

---
+++ linear_regression/batch_gd.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from .base import BaseRegression
+
+class LinearRegressionBatchGD(BaseRegression):
+    '''
+    Implements Batch Gradient Descent Linear Regression
+                y_hat = X.w + b
+    Where
+        X     - Input Features
+        y_hat - continous target variable
+        w     - weights/ coefficents
+        b     - bias/intercept
+    '''
+    def __init__(self, lr = 0.01, max_iter = 10000, verbose = None):
+        self.learning_rate = lr
+        self.max_iter = max_iter
+        self.verbose = verbose
+        self.loss_history_ = []
+        super().__init__()
+
+    def fit(self, X, y):
+        X = np.asarray(X)
+        y = np.asarray(y)
+        n, p = X.shape
+
+        #Initialize parameters
+        self.n_features_in_ = X.shape[1]
+        self.weights = np.zeros(p)
+        self.bias = 0.0
+
+        for itr in range(self.max_iter):
+
+            #Forward pass
+            y_hat = X @ self.weights + self.bias
+            #prediction error
+            error = y - y_hat
+            
+            # Compute gradients of the MSE loss
+            dw = -(2/n)* (X.T @ error)
+            db = -(2/n)* np.sum(error)
+
+            #Update parameters using gradient descent
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+            loss = np.mean(error ** 2)
+            self.loss_history_.append(loss)
+
+            if self.verbose and itr % self.verbose == 0:
+                print(f"Iteration {itr}, Loss: {loss:.6f}")
+
+        return self
